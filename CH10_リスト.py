ccb14153bb660d25f5c332ca71c7e1ce8f9abe05
koken3d4 <koken3d4@gmail.com>
2020-10-24 18:51:10 +0900

    CH10　リストについて

--- CH10_リスト.py
+++ CH10_リスト.py
@@ -2,18 +2,46 @@
 n = list(range(4))
 print(n)
 
-# newList=list()
+newList = list()
 t = "a textbook of Python"
 tlist = t.split()
 for str in tlist:
     print(str)
-    # newList.__add__(str)  #アッドはこれではない?なかなか思い通りには行かないものだ。
+    newList.append(str)  # add ではなく　appendが正。これは難しい。
 
 
-# print(newList[-2])
+print(newList[-2])
 print(tlist)
 
 
 # 歩の添え字
-a=[1,2,3,54,5]
+a = [1, 2, 3, 54, 5]
 print(a[-1])
+
+# スライス
+b = a[1:3]  # 3については戦闘からのインデックスでないことに注意。　長さでもなくて、後ろからのインデックスになる。
+
+print(b)
+
+# 二つのリストを統合するにはextendを用いる。
+a = [5, 1, 3, 2]
+b = [343, 21, 43]
+a.extend(b)
+print(a)
+
+
+# リストのリスト
+
+a = [[1, 2, 3], [4, 5, 6], [4, 6, 9]]
+sum = 0
+for row in a:
+    for element in row:
+        sum += element
+
+print(a)
+print(sum)
+
+
+# 内包表記というもの、Pythonのアルゴリズムでこのようなものが有ったように思えるが･･･。
+a = [i*i for i in range(5)]
+print(a)
