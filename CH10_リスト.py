4df21a0ea1519f670231293a4d6fe3d6871002dc
koken3d4 <koken3d4@gmail.com>
2020-10-24 17:45:11 +0900

    testでのコミットあとでunstageかする

---
+++ CH10_リスト.py
@@ -0,0 +1,19 @@
+# e = list()
+n = list(range(4))
+print(n)
+
+# newList=list()
+t = "a textbook of Python"
+tlist = t.split()
+for str in tlist:
+    print(str)
+    # newList.__add__(str)  #アッドはこれではない?なかなか思い通りには行かないものだ。
+
+
+# print(newList[-2])
+print(tlist)
+
+
+# 歩の添え字
+a=[1,2,3,54,5]
+print(a[-1])
